8894e3d4023e592f7beeebddd9b578c21126776f
KimberlySwinton <kimberlyswinton@gmail.com>
2019-07-11 04:37:57 -0400

    abstr and chk anml done

---
+++ src/animalKingdom/AbstractAnimal.java
@@ -0,0 +1,29 @@
+package animalKingdom;
+
+public abstract class abstractAnimal {
+    
+    // fields
+    public static int maxId = 0;
+    public int id;
+    public String name;
+    public int dYear;
+
+    // constructor
+    public abstractAnimal(String name, int dYear) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.dYear = dYear;
+
+    }
+
+    public abstract String getMove();
+
+    public abstract String getBreath();
+
+    public abstract String getReproduce();
+
+    public String getEat() {
+        return "NOMNOMNOM";
+    }
+}
---
+++ src/animalKingdom/CheckAnimal.java
@@ -0,0 +1,5 @@
+package animalKingdom;
+
+public interface CheckAnimal {
+    public boolean test(abstractAnimal a);
+}
